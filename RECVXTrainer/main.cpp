d97363facdecf249cfcdc82d71ca47e36f873b33
George-Honeywell <george.honeywell@live.co.uk>
2024-03-30 12:07:19 +0000

    Create the framework for the application window

--- RECVXTrainer/main.cpp
+++ RECVXTrainer/main.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
 #include <Windows.h>
+#include <tchar.h>
+#include <cassert>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int main()
 {
-	std::cout << "Application Started..." << std::endl;
-	std::cout << "Version v0.0.0" << std::endl;
+	HINSTANCE hInstance = GetModuleHandle(nullptr);
 
+	const wchar_t CLASS_NAME[] = L"RE:CVX Trainer v0.0.0";
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"RECVX Trainer", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 300, 200, NULL, NULL, hInstance, NULL);
+	
+	if (!hwnd)
+		return 0;
+
+	ShowWindow(hwnd, SW_SHOW);
+	UpdateWindow(hwnd);
+
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+		EndPaint(hwnd, &ps);
+	}
+	return 0;
+	}
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
